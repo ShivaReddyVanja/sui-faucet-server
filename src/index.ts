57ef78081bec8a06a1ffcabe5021f8cd84b845b6
ShivaReddyVanja <shivareddyvanja@gmail.com>
2025-07-29 12:16:03 +0530

    cookie-cross-origin

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ await syncAdminsFromEnv();
 await initRateLimiters();    
 
 
-const allowedOrigins = ['https://suicet.xyz', 'https://artiswap.xyz','https://sui-client.netlify.app/'];
+const allowedOrigins = ['https://suicet.xyz', 'https://artiswap.xyz','https://sui-client.netlify.app/',"http://localhost:3000"];
 
 app.use(cors({
   origin: function (origin, callback) {
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -67,7 +67,7 @@ router.post('/admin/login', async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
